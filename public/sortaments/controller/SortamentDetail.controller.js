4fd4aa9699d33c368aa49dc11105fbbc9ab87899
Viktor Bubnin <v.bubnin@bdo.ru>
2018-03-09 12:23:24 +0300

    sortament

--- public/sortaments/controller/SortamentDetail.controller.js
+++ public/sortaments/controller/SortamentDetail.controller.js
@@ -16,17 +16,35 @@ sap.ui.define([
     },
 
     onSortamentDownload: function () {
-      var text = 'Привет';
-      var filename = 'Сортамент';
-      var element = document.createElement('a');
-      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-      element.setAttribute('download', filename);
+      var oTable = this.byId('sortamentTable'),
+          aContexts = oTable.getSelectedContexts(),
+          oObject,
+          sFilename,
+          element,
+          sTextData = ',Ширина##SECTION_PROPERTY##MILLIMETERS,Толщина ребра жесткости##SECTION_PROPERTY##MILLIMETERS,Толщина полки##SECTION_PROPERTY##MILLIMETERS,Сопряжение ребра жесткости##SECTION_PROPERTY##MILLIMETERS,Название стандарта##OTHER##,Название сечения##OTHER##,Материал несущих конструкций##OTHER##,Горизонтальный центроид##SECTION_PROPERTY##MILLIMETERS,Высота##SECTION_PROPERTY##MILLIMETERS,Вертикальный центроид##SECTION_PROPERTY##MILLIMETERS\n';
+      aContexts.forEach(function (oContext) {
+        oObject = oContext.getObject();
+        sTextData += oObject.type + ','
+                   + oObject.b + ','
+                   + oObject.s + ','
+                   + oObject.t + ','
+                   + oObject.R1 + ','
+                   + 'Двутавр широкополочный по СТО АСЧМ 20-93' + ','
+                   + oObject.type + ','
+                   + '<По категории>,'
+                   + oObject.b/2 + ','
+                   + oObject.h + ','
+                   + oObject.h/2 + '\n';
+      }, this);
 
+      sFilename = 'Двутавр широкополочный по СТО АСЧМ 20-93';
+
+      element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(sTextData));
+      element.setAttribute('download', sFilename);
       element.style.display = 'none';
       document.body.appendChild(element);
-
       element.click();
-
       document.body.removeChild(element)
     }
 
