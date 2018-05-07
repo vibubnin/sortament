869790f40e91bea560b15e1b750bef45a63e703a
Viktor Bubnin <v.bubnin@bdo.ru>
2018-05-07 15:11:57 +0300

    es6 refactor main index.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-var express = require("express");
-var app = express();
-var Iconv  = require('iconv').Iconv
-var utf8Iconv = new Iconv('windows-1251', 'utf-8');
-var fileUpload = require('express-fileupload');
+const express = require("express");
+const Iconv  = require('iconv').Iconv;
+const utf8Iconv = new Iconv('windows-1251', 'utf-8');
+const fileUpload = require('express-fileupload');
+const iPort = 8000;
 
-app.use(fileUpload());
+let app = express();
  
 app.use(express.static(__dirname + "/public"));
 
-app.post('/upload', function(req, res) {
-	// var columns = req.files
-	//     .myFileUpload.data.toString()
-	//     .split('\n')[0]
-	//     .slice(1)
-	//     .split(',');
-	var data = utf8Iconv.convert(req.files.myFileUpload.data).toString();
-	console.log(data);
+app.use(fileUpload());
+
+app.post('/upload', (req, res) => {
+	let data = utf8Iconv.convert(req.files.myFileUpload.data).toString();
+	//console.log(data);
 	res.send(data);
 });
 
-app.listen(8000, function() {
-	console.log('Сервер запущен');
+app.listen(iPort, () => { 
+	console.log('The Server is running on port ' + iPort);
 }); 
