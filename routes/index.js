c619ea7afbac09e65fb8102f87165e99f3a07215
Viktor Bubnin <v.bubnin@bdo.ru>
2018-05-08 15:50:53 +0300

    feat: base routes

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Iconv  = require('iconv').Iconv;
+const utf8Iconv = new Iconv('windows-1251', 'utf-8');
+const router = express.Router();
+
+router.route('/api/sortaments')
+	.get((req, res) => {})
+	.post((req, res) => {})
+	.put((req, res) => {})
+	.delete((req, res) => {});
+
+router.route('/api/params')
+	.get((req, res) => {})
+	.post((req, res) => {})
+	.put((req, res) => {})
+	.delete((req, res) => {});
+
+router.route('/api/data')
+	.get((req, res) => {})
+	.post((req, res) => {})
+	.put((req, res) => {})
+	.delete((req, res) => {});
+
+router.route('/api/uploadFile')
+	.post((req, res) => {
+		let data = utf8Iconv.convert(req.files.myFileUpload.data).toString();
+		//console.log(data);
+		res.send(data);
+	});
+
+module.exports = router;
